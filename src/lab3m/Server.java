05b79c42b972d6ccd0676a74e52bf4761466644f
paradise999 <32838010+paradise999@users.noreply.github.com>
2017-11-28 22:04:25 +0200

    Initial commit

---
+++ src/lab3m/Server.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3m;
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author parad
+ */
+public class Server {
+    public static void main(String[] ar)    {
+     int port = 6666; // случайный порт 
+       try {
+         ServerSocket ss = new ServerSocket(port); // создаем сокет сервера и привязываем его к вышеуказанному порту
+         System.out.println("Waiting for a client...");
+
+         Socket socket = ss.accept(); // заставляем сервер ждать подключений и выводим сообщение когда кто-то связался с сервером
+         System.out.println("Got a client :) ... Finally, someone saw me through all the cover!");
+         System.out.println();
+
+ // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиенту. 
+         InputStream sin = socket.getInputStream();
+         OutputStream sout = socket.getOutputStream();
+
+ // Конвертируем потоки в другой тип, чтоб легче обрабатывать текстовые сообщения.
+         DataInputStream in = new DataInputStream(sin);
+         DataOutputStream out = new DataOutputStream(sout);
+
+         String line = null;
+         while(true) {
+           line = in.readUTF(); // ожидаем пока клиент пришлет строку текста.
+           if ("stop".equals(line)){ 
+               out.writeUTF(line);
+               out.flush();
+               System.out.println("Server stop");               
+               break;
+           }
+           System.out.println("The dumb client just sent me this line : " + line);
+           System.out.println("I'm sending it back...");
+           out.writeUTF(line); // отсылаем клиенту обратно ту самую строку текста.
+           out.flush(); // заставляем поток закончить передачу данных.
+           System.out.println("Waiting for the next line...");
+           System.out.println();
+         }
+      } catch(Exception x) { x.printStackTrace(); }
+   }
+}
